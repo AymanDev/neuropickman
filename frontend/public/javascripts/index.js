6d77557cd973e88beeadc0a78b1ea9b5c4d297f8
AymanDev <maxsa21@gmail.com>
2019-01-26 23:53:30 +0300

    Modal features and post requests
    Added modal design feature
    Created post requests

--- frontend/public/javascripts/index.js
+++ frontend/public/javascripts/index.js
@@ -1,6 +1,16 @@
 $(document).ready(() => {
-  $('#submit-btn').click((e) => {
+  $('#form').on('submit', (e) => {
     $('.modal').modal('show');
+    const data = $('#form').serializeArray();
+    $.post('localhost:8080/getAnalyze', data, (data) => {
+      console.log(data);
+      $('.modal').modal('hide');
+    });
     e.preventDefault();
   });
+  /*$('#submit-btn').click((e) => {
+    $.post('localhost:8080',);
+    $('.modal').modal('show');
+    e.preventDefault();
+  });*/
 });
