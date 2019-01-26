e3d4e058580e76f05f7ee3c9b0c95fd61bfbc4b4
AymanDev <maxsa21@gmail.com>
2019-01-27 00:40:34 +0300

    Remake requests
    Remaked requests from ajax to fetch

--- frontend/public/javascripts/index.js
+++ frontend/public/javascripts/index.js
@@ -2,21 +2,34 @@ $(document).ready(() => {
   $('#form').on('submit', (e) => {
     $('.modal').modal('show');
     const data = $('#form').serializeArray();
-    $.ajax({
-      url: 'localhost:8080/getAnalyze',
-      type: 'POST',
-      contentType: "application/json; charset=utf-8",
-      data: JSON.stringify(data),
-      dataType: 'jsonp',
-      success: (json) => {
-        console.log(json);
-        console.log('success');
+
+    fetch('http://localhost:8080/getAnalyze', {
+      headers: {
+        'content-type': 'application/json; charset=UTF-8'
       },
-      error: () => {
-        $('.modal').modal('hide');
-        console.log('fail');
-      }
-    });
+      body: data,
+      method: 'POST'
+    }).then(data => console.log(data))
+      .then(res => console.log(res))
+      .catch(err => console.error(err));
+    // $.post('localhost:8080/getAnalyze', data, (data, status) => {
+    //   console.log(data);
+    // });
+    // $.ajax({
+    //   url: 'localhost:8080/getAnalyze',
+    //   type: 'POST',
+    //   contentType: "application/json; charset=utf-8",
+    //   data: JSON.stringify(data[0]),
+    //   dataType: 'jsonp',
+    //   success: (json) => {
+    //     console.log(json);
+    //     console.log('success');
+    //   },
+    //   error: () => {
+    //     $('.modal').modal('hide');
+    //     console.log('fail');
+    //   }
+    // });
     // $.post('localhost:8080/getAnalyze', data, (data) => {
     //   console.log(data);
     //   $('.modal').modal('hide');
